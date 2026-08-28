557a37a5c90bfadcffdb0514d734bfc4938a50f9
opoooye123 <amnotpissed@gmail.com>
2026-08-28 22:34:48 +0100

    fixed

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -81,8 +81,23 @@ const optionalAuth = async (req, res, next) => {
 
   return next();
 };
+const adminOnly = (req, res, next) => {
+  if (!req.user) {
+    return res.status(401).json({
+      message: "Not authenticated",
+    });
+  }
 
+  if (req.user.role !== "admin") {
+    return res.status(403).json({
+      message: "Admin access required",
+    });
+  }
+
+  next();
+};
 module.exports = {
   protect,
   optionalAuth,
+  adminOnly
 };
